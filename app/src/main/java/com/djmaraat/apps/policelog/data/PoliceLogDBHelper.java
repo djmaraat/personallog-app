4c5f84dfbb9cb5455a956bce0fd0177dd0a18c2c
Dandeljane Maraat <djmaraat@gmail.com>
2016-10-22 13:02:40 +0800

    copyright info added

--- app/src/main/java/com/djmaraat/apps/policelog/data/PoliceLogDBHelper.java
+++ app/src/main/java/com/djmaraat/apps/policelog/data/PoliceLogDBHelper.java
@@ -1,3 +1,13 @@
+/*
+ *
+ * Copyright 2016 Dandeljane Maraat
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+
 package com.djmaraat.apps.policelog.data;
 
 import android.content.ContentValues;
@@ -9,9 +19,6 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import java.util.ArrayList;
 
-/**
- * Created by Dandeljane Maraat on 12/10/2016.
- */
 public class PoliceLogDBHelper extends SQLiteOpenHelper {
     // If you change the database schema, you must increment the database version.
     public static final int DATABASE_VERSION = 1;
